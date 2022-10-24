0c7cc2f61b903815421c5c46f0a10dabec3d2016
Oraange <hrpp1300@naver.com>
2022-10-24 09:41:36 +0900

    Feat: Change sync to async
    - 비동기 컨슈머 구현

--- chat/routing.py
+++ chat/routing.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
 
-from . import consumers
+from . import async_consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<room_name>\w+)/$", async_consumers.ChatConsumer.as_asgi()),
 ]
